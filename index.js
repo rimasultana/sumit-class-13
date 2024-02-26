dedfae13b6ba5f069323b5ce5d8501ab4e85d6a1
rimasultana <rimasultana20225@gmail.com>
2024-02-26 17:12:50 +0600

    function

---
+++ index.js
@@ -0,0 +1,12 @@
+const car = {
+    name: "Flot";
+    model: 800;
+    width: 850kg;
+    color: white;
+    start: function () {
+        console.log("car has started");
+    }, drive: function{
+    console.log() ("car is driving");
+},
+},
+console.log(model);
